d55b6a29a2f41ae45d58b7d505abac8b39399c3c
Dora Belme <contact.dorabelme@gmail.com>
2020-02-14 13:35:47 -0500

    correct / incorrect answer display logic

--- game.js
+++ game.js
@@ -44,7 +44,6 @@ const startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestion();
 
 };
@@ -76,8 +75,21 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
-        console.log(selectedAnswer);
-        getNewQuestion();
+        
+        // const classToApply = 'incorrect';
+        // if (selectedAnswer == currentQuestion.answer) {
+        //     classToApply = 'correct';
+        // }
+
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
     });
 });
 
